063cca4b99b4d1aad50b1622dd13c4cc52128431
Shahbaz Khalid <130798658+shahbazkhaliddev786@users.noreply.github.com>
2024-07-10 13:11:24 +0500

    Update Form.tsx

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -40,7 +40,7 @@ export default function PromptForm({ chatId, setMessages }: PromptFormProps) {
     setContent("");
 
     try {
-      const res = await fetch(`http://localhost:3000/api/messages/${chatId}`, {
+      const res = await fetch(`/api/messages/${chatId}`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
